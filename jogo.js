ec243f6e69508a7ec7e40be3a64367fb4ea80dec
Yuri Almeida <yurialves2@gmail.com>
2020-03-22 00:35:17 -0300

    drawing flappybird

--- jogo.js
+++ jogo.js
@@ -5,6 +5,26 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
+// [Pássaro]
+const flappyBird = {
+  spriteX: 0,
+  spriteY: 0,
+  width: 33,
+  height: 24,
+  x: 10,
+  y: 50,
+  draw() {
+    context.drawImage(
+      sprites,
+      this.spriteX, this.spriteY,
+      this.width, this.height,
+      this.x, this.y,
+      this.width, this.height,
+    );
+  }
+};
+
 (function loop() {
+  flappyBird.draw();
   requestAnimationFrame(loop);
 })();
