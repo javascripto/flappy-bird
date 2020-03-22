09f54b47b34cdbbdf30f921963b5b6af4ba01b9f
Yuri Almeida <yurialves2@gmail.com>
2020-03-22 00:35:57 -0300

    added floor

--- jogo.js
+++ jogo.js
@@ -5,6 +5,32 @@ sprites.src = './sprites.png';
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
+// [Chão]
+const floor = {
+  spriteX: 0,
+  spriteY: 610,
+  width: 224,
+  height: 112,
+  x: 0,
+  y: canvas.height - 112,
+  draw() {
+    context.drawImage(
+      sprites,
+      this.spriteX, this.spriteY,
+      this.width, this.height,
+      this.x, this.y,
+      this.width, this.height,
+    );
+    context.drawImage(
+      sprites,
+      this.spriteX, this.spriteY,
+      this.width, this.height,
+      (this.x + this.width), this.y,
+      this.width, this.height,
+    );
+  }
+};
+
 // [Pássaro]
 const flappyBird = {
   spriteX: 0,
@@ -25,6 +51,7 @@ const flappyBird = {
 };
 
 (function loop() {
+  floor.draw();
   flappyBird.draw();
   requestAnimationFrame(loop);
 })();
